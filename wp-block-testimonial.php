bdf2dea3a9dd3d3204d3620a4ee66bc37662ebe8
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2021-12-29 18:08:57 +0200

    bump 2.0.1

--- wp-block-testimonial.php
+++ wp-block-testimonial.php
@@ -4,7 +4,7 @@
  * Description:       Shares feedback, review etc. of other people.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           2.0.0
+ * Version:           2.0.1
  * Author:            Innocode
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
